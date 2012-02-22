7eb333f42d45241f389512a074829537ebc27511
Ryan Daigle <ryan.daigle@gmail.com>
2012-02-22 09:51:43 -0500

    Properly set env in Procfile

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 heroku create -s cedar --addons memcache,newrelic
 git push heroku master
-cat .env | tr '\n' ' ' | xargs heroku config:add 
+cat .env | grep -v 'RACK_ENV' | tr '\n' ' ' | xargs heroku config:add 
 heroku open
