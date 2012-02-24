9b0f80cd1a2d61574f0a1b5c533b2c549e38fdab
Ryan Daigle <ryan.daigle@gmail.com>
2012-02-23 19:25:15 -0500

    Update theme. Update update script to do an upstream merge vs. rebase

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git commit -m "Commit changes prior to template update"
 git checkout upstream
 git pull upstream master
 git checkout master
-git rebase upstream
+git merge upstream
 cd themes/clean
 git pull origin master
 cd ../..
